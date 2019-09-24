884f92c46c1fab6903fea01e40fd0184aa7ddf43
Lee Fitchett <lefitche@microsoft.com>
2019-09-24 15:34:49 -0700

    convert logs to string

--- src/future.rs
+++ src/future.rs
@@ -1,3 +1,40 @@
-    pub fn test_main(){
-        println!("Hi", );
+extern crate futures;
+
+use std::{thread, time};
+
+use futures::future;
+use futures::future::Map;
+use futures::prelude::*;
+
+pub fn test_main() {
+    println!("Start");
+
+    let a = future::ok::<i32, i32>(1);
+    let b = future::ok::<i32, i32>(5);
+
+    let x = pause(a).and_then(|x| {future::ok(x + 5)});
+    let y = pause(b);
+
+    println!("after pause", );
+
+    let xy = x.join(y);
+
+    if let Ok((xval, yval)) = xy.wait() {
+        println!("x: {}, y: {}", xval, yval);
     }
+}
+
+fn pause<F>(future: F) -> Map<F, fn(i32) -> i32>
+where
+    F: Future<Item = i32>,
+{
+    fn add(a: i32) -> i32 {
+        println!("Waiting {} seconds.", a);
+
+        // let test = future::ok(1);
+        thread::sleep(time::Duration::from_millis(a as u64));
+        println!("Waited {} seconds.", a);
+        a + 10
+    }
+    future.map(add)
+}
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,17 @@ mod random;
 mod future;
 
 fn main() {
+
+use std::str;
+
+// some bytes, in a vector
+let sparkle_heart = vec![240, 159, 146, 150];
+
+// We know these bytes are valid, so just use `unwrap()`.
+let sparkle_heart = str::from_utf8(&sparkle_heart).unwrap();
+
+assert_eq!("💖", sparkle_heart);
+
+
     future::test_main();
 }
