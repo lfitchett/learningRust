e144cf66d364d778b82fdedf2e1f2d40c1f1f524
lfitchett <lfitchett1@gmail.com>
2020-03-13 17:45:31 -0700

    remove all

--- src/advent/day2_2.rs
+++
@@ -1,7 +0,0 @@
-// use super::file;
-
-pub(crate) fn main() {
-    // let file = file::file_read("data/2.txt");
-    // let lines = file.lines();
-
-}
--- src/advent/mod.rs
+++
@@ -1,8 +0,0 @@
-mod file;
-
-pub(crate) mod day1;
-pub(crate) mod day2_1;
-pub(crate) mod day2_2;
-pub(crate) mod day3;
-pub(crate) mod day4;
-pub(crate) mod day5;
